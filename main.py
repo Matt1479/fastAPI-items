6e05409779d4b06b0684cb5ee81f565076e6dc75
Matt1479 <gitmatt1479@gmail.com>
2025-12-31 14:34:17 +0100

    Feature(main.py): Define ITEMS as a global variable; Get items from API; Implement GET /items/.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from models import Item
+from utils import get_items_from_api
 
+
+ITEMS: list[Item] = get_items_from_api()
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "hello, world!"}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    return {"items": ITEMS[skip : skip + limit]}
