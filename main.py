81359e9b8e1399ccedd682df1187d383c41d1082
Matt1479 <gitmatt1479@gmail.com>
2025-12-31 15:43:59 +0100

    Feature(main.py): Implement PUT /items/update/{item_id}.

--- main.py
+++ main.py
@@ -33,3 +33,13 @@ async def read_item(item_id: int):
 @app.get("/items/")
 async def read_items(skip: int = 0, limit: int = 10):
     return ITEMS[skip : skip + limit]
+
+
+@app.put("/items/update/{item_id}")
+async def update_item(item_id: int, item: Item):
+    for i, _item in enumerate(ITEMS):
+        if _item.id == item_id:
+            ITEMS[i] = Item(**item.model_dump())
+            ITEMS[i].id = item_id
+            return ITEMS[i]
+    return {"error": "not found"}
