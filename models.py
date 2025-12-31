bcf276df3d8ef4ef736898826628d95b2cfcfb8d
Matt1479 <gitmatt1479@gmail.com>
2025-12-31 14:58:51 +0100

    Refactor(models.py): Add some default/optional values to Item's attributes/properties.

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 
 class Item(BaseModel):
-    id: int
+    id: int = 0
     title: str
     description: str | None = None
     category: str | None = None
-    price: float
-    stock: int
+    price: float = 0
+    stock: int = 0
